895b12fe020304a38a49aab0b79398e2b92a2992
den766 <asharafalika886@gmail.com>
2026-09-07 15:00:58 +0530

    chore: prepare backend for deployment

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ console.log(expenses);
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -111,6 +111,6 @@ app.delete("/expenses/:id", (req, res) => {
   res.status(204).send();
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
